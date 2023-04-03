446b99eac870907cbb5dd1b645e7f977662b026d
tyson lind <tysonfloydlind@gmail.com>
2023-04-03 09:46:50 -0600

    completed assignment

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite color?");
+            var userColor = Console.ReadLine();
+            switch (userColor)
+            {
+                case "blue":
+                    Console.WriteLine("Your favorite color was blue");
+                        break;
+                case "red":
+                    Console.WriteLine("Your favorite color was red");
+                    break;
+                default:
+                    Console.WriteLine("Your favorite color was not one of the switch cases");
+                    break;
+
+            }
         }
     }
 }
